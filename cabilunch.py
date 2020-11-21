16fa82bad9dba5f5cd21003cd22faab6ebe0cc7b
jaimedefermin <47278792+jaimedefermin@users.noreply.github.com>
2020-11-21 18:38:21 +0100

    Add files via upload
    creates groups with random leaders, but does not randomize the groups when creating them.

--- cabilunch.py
+++ cabilunch.py
@@ -20,44 +20,7 @@ counting = False
 available=[]
 lunchgroups = []
 groups = 0
-
-class GroupMessage:
-    START_TEXT = {
-        'type' : 'section',
-        'text' : {
-            'type': 'mrkdwn',
-            'text':(
-                'This will be your lunch group for today. \n\n'
-                '*{} is in charge of making the reservations.*'.format(available)
-            )
-        }
-    }
-
-    DIVIDER = {'type': 'divider'}
-
-
-    def __init__(self, channel, users):
-        self.channel = channel
-        self.users = users
-        self.icon_emoji = ':ramen:'
-        self.timestamp = ''
-
-    def get_message(self):
-        return {
-            'ts' : self.timestamp,
-            'channel': self.channel,
-            'username' : 'Lunch Bot',
-            'icon_emoji': self.icon_emoji,
-            'leader' : self.users[0],
-            'blocks':[
-                self.START_TEXT,
-                self.DIVIDER
-            ]
-        }
-    
-    
-
-    
+   
 
 @slack_event_adapter.on('message')
 def message(payLoad):
@@ -74,20 +37,22 @@ def message(payLoad):
             client.chat_postMessage(channel=channel_id, text=f'<@{user_id}> said yes')
             printable_users = [f"<@{user}> " for user in available]
             msg = "".join(printable_users)
-            client.chat_postMessage(channel=channel_id, text=f'{msg}are comming')
+            if len(available)==1:
+                client.chat_postMessage(channel=channel_id, text=f'{msg} is comming')
+            else:
+                client.chat_postMessage(channel=channel_id, text=f'{msg} are comming')
 
 
 #@app.shortcut("create_conversation")
-def send_group_to_groupchat(users):
-    #gm= GroupMessage(channel,users)
-    #message = gm.get_message()
-    #response = client.chat_postMessage(**message)
-    #gm.timestamp = response['ts']
-
-    global available
-    print(users)
-    client.conversations_open(users= users)
-
+def send_group_to_groupchat(n, users):
+    channel_name = f'lunch-group-{n}'
+    response = client.conversations_create(name = channel_name, user_ids = users)
+    channel_id = response["channel"]["id"]
+    response = client.conversations_invite(channel=channel_id, users=users)
+    leader = random.randint(0, len(users)-1)
+    client.chat_postMessage(channel=channel_id, text=f'This is lunchgroup number {n}')
+    client.chat_postMessage(channel=channel_id, text=f'The person in charge of making the reservation is <@{users[leader]}>')
+    
 
 
 
@@ -97,7 +62,7 @@ def lunch_time():
     global counting
     counting = True
     data =request.form
-    user_id = data.get('user_id')
+    #user_id = data.get('user_id')
     #print(data)
     channel_id =data.get('channel_id')
     client.chat_postMessage(channel=channel_id, text="""Hello, who's having lunch out today? Please reply "me" or "yes" if you would """)
@@ -107,73 +72,55 @@ def lunch_time():
 def no_more_people():
     global groups
     global lunchgroups
+    global available
+    global counting
+
     data =request.form
     channel_id =data.get('channel_id')
-    user_id = data.get('user_id')
-    global counting
-    counting = False
-    global available
-    
-    #response = client.conversations_open(users=available)
+    #user_id = data.get('user_id')
 
-    nlunchers = len(available)
-    if nlunchers <= 7:
-        groups = 1
-    elif nlunchers % 7 == 0:
-        groups = nlunchers/7
+    if counting == False:
+        client.chat_postMessage(channel=channel_id, text= "You forgot to ask who was coming to lunch. Please type /lunch-time if you wish to ask the group :)")
+    
     else:
-        groups = int(nlunchers/7) +1
-    
-    ppl = int(nlunchers/groups)
-    leftovers = nlunchers - ppl*groups
-    index=0
-
-    for i in range(0,groups):
-        lunchgroups.append([])
-        for j in range (0, ppl): 
-            lunchgroups[i].append(available[index])
-            index+=1
-    if leftovers > 0:
-        for i in range(0, leftovers):
-            lunchgroups[i].append(available[ppl*groups + i+1])
-
-            
-                
-
-
-
-
-    client.chat_postMessage(channel=channel_id, text="""We are no longer counting""")
-
-    #debug
-    for i in range(0,len(lunchgroups)):
-            send_group_to_groupchat(lunchgroups[i])
-
-
-
-    for i in range(1, groups+1):
-        client.chat_postMessage(channel=channel_id, text="""The groups are as follows :""")
-        gp = lunchgroups[i-1]
-        client.chat_postMessage(channel=channel_id, text=f'Group{i}: ')
-        for j in range(0,len(gp)):
-            client.chat_postMessage(channel=channel_id, text=f'<@{gp[j]}>')
-        leader=random.randint(0,len(gp))
-        client.chat_postMessage(channel=channel_id, text=f'Leader for Group{i}: <@{gp[leader]}>')
-
+        counting = False
         
-    if nlunchers  < 1:
-        client.chat_postMessage(channel=channel_id, text='No one is going out for lunch today')
-    elif nlunchers == 1:
-        client.chat_postMessage(channel=channel_id, text=f'Only <@{available[0]}> is having lunch out today :(')
-        send_group_to_groupchat(available[0])
-        #send_group_to_groupchat(f'@{available[0]}', available)
-    else:
-        client.chat_postMessage(channel=channel_id, text=f'{nlunchers} people are coming')
-        for i in range(0, len(lunchgroups)):
-            send_group_to_groupchat(lunchgroups[i])
-        #send_group_to_groupchat(f'@{available[0]}', available)
+        
 
-    available.clear()
+        nlunchers = len(available)
+        if nlunchers <= 7:
+            groups = 1
+        elif nlunchers % 7 == 0:
+            groups = nlunchers/7
+        else:
+            groups = int(nlunchers/7) +1
+        
+        ppl = int(nlunchers/groups)
+        leftovers = nlunchers - ppl*groups
+        index=0
+
+        for i in range(0,groups):
+            lunchgroups.append([])
+            for _ in range (0, ppl): 
+                lunchgroups[i].append(available[index])
+                index+=1
+        if leftovers > 0:
+            for i in range(0, leftovers):
+                lunchgroups[i].append(available[ppl*groups + i+1])
+
+
+        client.chat_postMessage(channel=channel_id, text="""We are no longer counting""")
+    
+        if nlunchers  < 1:
+            client.chat_postMessage(channel=channel_id, text='No one is going out for lunch today')
+        elif nlunchers == 1:
+            client.chat_postMessage(channel=channel_id, text=f'Only <@{available[0]}> is having lunch out today :(')
+            send_group_to_groupchat(1, lunchgroups[0])
+        else:
+            client.chat_postMessage(channel=channel_id, text=f'{nlunchers} people are coming')
+            for i in range(0, len(lunchgroups)-1):
+                send_group_to_groupchat(i+1, lunchgroups[i])   
+        available.clear()
     return Response(), 200
 
 
